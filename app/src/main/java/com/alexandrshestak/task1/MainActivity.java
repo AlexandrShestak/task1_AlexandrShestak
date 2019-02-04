95fe0a6f2873655f1750280d49037dd0ab6b6bce
AlexandrShestak <shestakalexandr@mail.ru>
2019-02-05 00:26:41 +0300

    feature: add App Center Distribute functionality

--- app/src/main/java/com/alexandrshestak/task1/MainActivity.java
+++ app/src/main/java/com/alexandrshestak/task1/MainActivity.java
@@ -4,6 +4,9 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import com.crashlytics.android.Crashlytics;
+import com.microsoft.appcenter.AppCenter;
+import com.microsoft.appcenter.distribute.Distribute;
+
 import io.fabric.sdk.android.Fabric;
 
 /**
@@ -11,10 +14,14 @@ import io.fabric.sdk.android.Fabric;
  */
 public class MainActivity extends AppCompatActivity {
 
+    private static final String APP_CENTER_KEY = "43160772-9689-4752-8b19-eb89945e78b8";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         Fabric.with(this, new Crashlytics());
+        AppCenter.start(getApplication(), APP_CENTER_KEY, Distribute.class);
+
         setContentView(R.layout.activity_main);
     }
 
